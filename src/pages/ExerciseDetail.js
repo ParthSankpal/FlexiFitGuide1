1fa923f2ada62e3a86d94ec51f7992d0ec086317
ParthSankpal <parth.sankpal47@gmail.com>
2023-11-23 12:51:49 +0530

    Updated UI

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -19,6 +19,7 @@ const ExerciseDetail = () => {
 
     const fetchExercisesData = async () => {
       const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+      // Youtube Search and Download API 
       const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
 
       const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
